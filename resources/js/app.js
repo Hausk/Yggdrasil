525da3c0a30b6b7c6579ed874ca4c780aa3ab441
Hausk <hauskwar@gmail.com>
2023-05-24 12:47:51 +0200

    Config: Ajout de Headlessui & fontawesome dans les libs + Mise en place d'une navbar à modifier après le back

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,19 @@
 import './bootstrap';
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faChevronDown, faChevronUp, faUserGear } from '@fortawesome/free-solid-svg-icons'
+
 import PublicNavBar from './components/public/NavBar.vue';
 
+/* add icons to the library */
+library.add(faChevronDown, faChevronUp, faUserGear);
 const app = createApp({});
 
 app.component('publicNavBar', PublicNavBar);
-
+app.component('fontAwesomeIcon', FontAwesomeIcon);
 app.mount('#app');
